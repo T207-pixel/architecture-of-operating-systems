43b32de9a522a88a5b73a042b3181ac7fb328c1b
Timofey Novikov <timofey.i.novikov@openelbrus.ru>
2024-11-10 18:08:17 +0300

    TASK4: Sleep orphan

---
+++ lab2/4.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void printInfo() {
+    pid_t pid, ppid; // PID - process id, PPID - parent process id.
+    gid_t gid;
+    
+    pid = getpid();
+    ppid = getppid();
+    gid = getgid();
+    printf("pid=%d\nppid=%d\ngid=%d\n\n", pid, ppid, gid);
+}
+
+int main() {
+    int id;
+    int status;
+    int parrent = getpid();
+    
+    printf("Before fork\n");
+    printInfo();
+    
+    id = fork(); // create new process
+    printf("After fork\n");
+    printInfo();
+
+    int n;
+    if (id == 0) {
+        n = 1;
+        printf("Child\n\n");
+    } else {
+        n = 6;
+        printf("Parent\n\n");
+    }
+
+    if (id == 0) { // allow child process to finish without interuptions from main process id != 0
+        sleep(8);
+        printInfo();
+    }
+
+    for (int i = n; i < n + 5; i++) {
+        printf("%d ", i);
+        sleep(1);
+        fflush(stdout);
+    }
+
+    printf("\n");
+    return 0;
+}

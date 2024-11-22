643743301392ed570fa55e8470ba37b7d904fbc6
Timofey Novikov <timofey.i.novikov@openelbrus.ru>
2024-11-22 17:58:31 +0300

    IMPR: [7.c]

---
+++ lab2/7.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void printInfo() {
+    pid_t pid, ppid; // PID - process id, PPID - parent process id.
+    gid_t gid;
+    
+    pid = getpid();
+    ppid = getppid();
+    gid = getgid();
+    printf("pid=%d\nppid=%d\ngid=%d\n\n", pid, ppid, gid);
+}
+
+int main() {
+    int id;
+    int status;
+    int parrent = getpid();
+    
+    printf("Before fork\n");
+    printInfo();
+    
+    id = fork(); // create new process
+    printf("After fork\n");
+    printInfo();
+
+    if (id == 0) {
+        setpgrp();
+    }
+
+    pause();
+
+    return 0;
+}
---
+++ lab2/7.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+gcc 7.c
+./a.out

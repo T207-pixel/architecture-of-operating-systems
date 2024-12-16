25794cf73a15907792889258194482e81d9a9e70
Timofey Novikov <timofey.i.novikov@openelbrus.ru>
2024-12-16 17:25:45 +0300

    RFCT: [lab3] 10.c SIGUSR1, SIGUSR2

--- lab3/10.c
+++ lab3/10.c
@@ -8,24 +8,14 @@
 #include <signal.h>
 #include <limits.h>
 
-void handle_signal(int sig) {
-    if (sig == SIGINT) {
-        printf("caught SIGINT\n");
-    } else {
-        signal(sig, SIG_DFL);
-    }
-}
-
 int main() {
-    signal(SIGINT, handle_signal);
-
     sigset_t sigset;
     sigemptyset(&sigset);
-    sigaddset(&sigset, SIGINT);
-    sigaddset(&sigset, SIGALRM);
+    sigaddset(&sigset, SIGUSR1);
+    sigaddset(&sigset, SIGUSR2);
     sigprocmask(SIG_BLOCK, &sigset, NULL);
 
-    printf("Try to do Ctrl -c or try to send -ALRM <PID>\n");
+    printf("Blocked\nTry to execute command:\nkill -USR1 <PID>\nkill -USR2 <PID>\n");
     sleep(30);
     printf("Unblocked.\n");
     sigprocmask(SIG_UNBLOCK, &sigset, NULL);

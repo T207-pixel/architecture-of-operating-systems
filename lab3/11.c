ae3684f4b304b114aa17b9fb1993f15caf8230fd
Timofey Novikov <timofey.i.novikov@openelbrus.ru>
2024-12-16 20:22:20 +0300

    11.c

--- lab3/11.c
+++ lab3/11.c
@@ -12,36 +12,46 @@
 
 void synced(pid_t other_pid) {
     pid_t this_pid = getpid();
-    sigset_t sigset, e_sigset;
+    sigset_t sigset;
+    sigset_t  empty_sigset;
+
     sigaddset(&sigset, SIGUSR1);
-    sigemptyset(&e_sigset);
+    sigemptyset(&empty_sigset);
     sigprocmask(SIG_BLOCK, &sigset, NULL);
 
-    if (this_pid < other_pid) sigsuspend(&e_sigset);
+    if (this_pid < other_pid) {
+        sigsuspend(&empty_sigset);
+    }
 
     while(1) {
         printf("%d\n", this_pid);
-        sleep(rand() % 5);
+        sleep(2);
         kill(other_pid, SIGUSR1);
-        sigsuspend(&e_sigset);
+        sigsuspend(&empty_sigset);
     }
 }
 
-void disp(int sig) {
-    if (sig == SIGUSR1) ;
-    else SIG_DFL;
+void signal_handler(int sig) {
+    if (sig == SIGUSR1) {
+
+    };
 }
 
 int main() {
     struct sigaction sa;
-    sa.sa_handler = disp;
-    sa.sa_flags = 0;
+    sa.sa_handler = signal_handler;
     sigemptyset(&sa.sa_mask);
     sigaction(SIGUSR1, &sa, NULL);
 
-    int parent_pid = getpid(), child_pid;
-    if ((child_pid = fork()) == 0) synced(parent_pid);
-    else synced(child_pid);
+    int parent_pid = getpid();
+    int child_pid = fork();
+    
+    if ((child_pid) == 0) {
+        synced(parent_pid);
+    }
+    else {
+        synced(child_pid);
+    }
 
     return 0;
 }
